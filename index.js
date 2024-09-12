1d32c05ae40e23ae2a6c30daf3a3f91d0a4ba56f
Yash Raj <yashchoudhary10a@gmail.com>
2024-09-12 14:43:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+// I'm Feeling Lucky Button Functionality
+document.querySelector("input[value='I\'m Feeling Lucky']").addEventListener('click', function () {
+    window.location.href = "https://www.google.com/doodles"; 
+});
